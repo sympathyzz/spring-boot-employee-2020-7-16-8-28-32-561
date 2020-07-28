5d41d39d7569d3992bfe48099b71dc2617f12fa6
GAOGA4 <961977096@qq.com>
2020-07-28 22:48:02 +0800

    [Gavin] feature: implement getEmployees method

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeControl.java
@@ -0,0 +1,33 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeControl {
+    public static List<Employee> employees=new ArrayList<>();
+    public List<Employee> getAllData(){
+        Employee employee1=new Employee(1,"alibaba1",17,"male",6000);
+        Employee employee2=new Employee(2,"alibaba2",18,"female",7000);
+        Employee employee3=new Employee(3,"oocl1",19,"male",8000);
+        Employee employee4=new Employee(4,"oocl2",20,"female",9000);
+        if(employees.size()==0){
+            employees.add(employee1);
+            employees.add(employee2);
+            employees.add(employee3);
+            employees.add(employee4);
+        }
+        return employees;
+    }
+
+    @GetMapping("")
+    public List<Employee> getEmployees(){
+        return getAllData();
+    }
+}
