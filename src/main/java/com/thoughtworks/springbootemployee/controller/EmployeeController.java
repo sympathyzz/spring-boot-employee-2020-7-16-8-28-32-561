ca2aaa66484d5cc192cab7323fd3e46e32307ff8
GAOGA4 <961977096@qq.com>
2020-07-30 01:57:41 +0800

    [Gavin] feature:complete EmployeeController

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.Service.EmployeeService;
 import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +15,10 @@ import java.util.stream.Collectors;
 @RequestMapping("/employees")
 public class EmployeeController {
     private static List<Employee> employees=new ArrayList<>();
+
+    @Autowired
+    EmployeeService employeeService;
+
     public List<Employee> getAllData(){
         Employee employee1=new Employee(1,"Gavin",17,"male",6000);
         Employee employee2=new Employee(2,"Zach",18,"female",7000);
@@ -26,53 +33,40 @@ public class EmployeeController {
         return employees;
     }
 
-    @GetMapping
+    @GetMapping(params = {"page","pageNum"})
     @ResponseStatus(HttpStatus.OK)
-    public List<Employee> getEmployees(@RequestParam(defaultValue="0") Integer page, @RequestParam(defaultValue="0") Integer pageSize,@RequestParam(required = false)String gender){
-        if(gender!=null){
-            return getAllData().stream().filter(employee -> employee.getGender().equals(gender)).collect(Collectors.toList());
-        }
-        if(page!=0&&pageSize!=0){
-            return getAllData().stream().skip((page-1)*pageSize).limit(pageSize).collect(Collectors.toList());
-        }else{
-            return getAllData();
-        }
-
+    public Page<Employee> getEmployees(@RequestParam Integer page, @RequestParam Integer pageSize){
+            return employeeService.getEmployeeByPage(page,pageSize);
     }
+
+    @GetMapping(params = "gender")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Employee> getEmployees(@RequestParam String gender){
+        return employeeService.getEmployeeByGender(gender);
+    }
+
     @GetMapping("/{name}")
     @ResponseStatus(HttpStatus.OK)
     public Employee getSpecifiedNameEmployee(@PathVariable String name){
-        return getAllData().stream().filter(employee ->employee.getName().equals(name)).collect(Collectors.toList()).get(0);
+        return employeeService.findByName(name);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Employee addEmployee(Employee employee){
-        if(getAllData().add(employee)){
-            return employee;
-        }
-        return null;
+        return  employeeService.addEmployee(employee);
     }
 
-    @PutMapping("/{employeeName}")
+    @PutMapping("/{employeeId}")
     @ResponseStatus(HttpStatus.OK)
-    public Employee updateEmployee(@PathVariable String employeeName,@RequestBody Employee employee){
-        Employee specifiedEmployee= getSpecifiedNameEmployee(employeeName);
-            if(employee!=null){
-                specifiedEmployee.setName(employee.getName());
-                specifiedEmployee.setId(employee.getId());
-                specifiedEmployee.setGender(employee.getGender());
-                specifiedEmployee.setSalary(employee.getSalary());
-                specifiedEmployee.setAge(employee.getAge());
-                return specifiedEmployee;
-            }
-        return null;
+    public Employee updateEmployee(@PathVariable Integer employeeId,@RequestBody Employee employee){
+        return employeeService.update(employeeId,employee);
     }
 
-    @DeleteMapping("/{employeeName}")
+    @DeleteMapping("/{employeeId}")
     @ResponseStatus(HttpStatus.OK)
-    public void deleteEmployee(@PathVariable String employeeName){
-        getAllData().remove(getSpecifiedNameEmployee(employeeName));
+    public void deleteEmployee(@PathVariable Integer employeeId){
+        employeeService.deleteEmployeeById(employeeId);
     }
 
 }
