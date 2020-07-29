1870f130d2b1a6f350d6f449a61bfb444b7895de
GAOGA4 <961977096@qq.com>
2020-07-29 14:40:35 +0800

    [Gavin,Zach] feature: add should_return_employee_when_update_given_employee_id test

---
+++ src/main/java/com/thoughtworks/springbootemployee/Service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.springbootemployee.Service;
+
+import com.thoughtworks.springbootemployee.controller.EmployeeController;
+import com.thoughtworks.springbootemployee.model.Employee;
+
+import java.util.List;
+
+public class EmployeeService {
+    public Employee update(int id) {
+        return null;
+    }
+}
---
+++ src/test/java/com/thoughtworks/springbootemployee/Service/EmployeeServiceTest.java
@@ -0,0 +1,23 @@
+package com.thoughtworks.springbootemployee.Service;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class EmployeeServiceTest {
+    @Test
+    void should_return_employee_when_update_given_employee_id(){
+        //given
+        EmployeeService employeeService = new EmployeeService();
+        Employee newEmployee= new Employee(1,"zach",18,"male",1000);
+
+        //when
+        Employee returnEmployee = employeeService.update(newEmployee.getId());
+
+        //then
+        assertEquals(returnEmployee,newEmployee);
+    }
+
+}
