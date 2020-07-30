394da6112580af889e8bdbf39b00ab836ea05140
GAOGA4 <961977096@qq.com>
2020-07-30 15:36:03 +0800

    [Gavin, Quentin] test:add should_throw_NoSuchDataException_when_update_given_wrong_company_id

--- src/test/java/com/thoughtworks/springbootemployee/Service/CompanyServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/Service/CompanyServiceTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.springbootemployee.Service;
 
+import com.thoughtworks.springbootemployee.exception.NoSuchDataException;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.respository.CompanyRepository;
@@ -12,26 +13,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 
 public class CompanyServiceTest {
 
-    private List<Company> generateCompanyList(){
-         List<Company> companies=new ArrayList<>();
-        Employee employee1=new Employee(1,"alibaba1",17,"male",6000);
-        Employee employee2=new Employee(2,"alibaba2",18,"female",7000);
-        Employee employee3=new Employee(3,"oocl1",19,"male",8000);
-        Employee employee4=new Employee(4,"oocl2",20,"female",9000);
-        List<Employee> alibabaEmployees=new ArrayList<>();
+    private List<Company> generateCompanyList() {
+        List<Company> companies = new ArrayList<>();
+        Employee employee1 = new Employee(1, "alibaba1", 17, "male", 6000);
+        Employee employee2 = new Employee(2, "alibaba2", 18, "female", 7000);
+        Employee employee3 = new Employee(3, "oocl1", 19, "male", 8000);
+        Employee employee4 = new Employee(4, "oocl2", 20, "female", 9000);
+        List<Employee> alibabaEmployees = new ArrayList<>();
         alibabaEmployees.add(employee1);
         alibabaEmployees.add(employee2);
-        List<Employee> OOCLEmployees=new ArrayList<>();
+        List<Employee> OOCLEmployees = new ArrayList<>();
         OOCLEmployees.add(employee3);
         OOCLEmployees.add(employee4);
-        Company company1=new Company(1,"alibaba",2,alibabaEmployees);
-        Company company2=new Company(2,"oocl",2,OOCLEmployees);
+        Company company1 = new Company(1, "alibaba", 2, alibabaEmployees);
+        Company company2 = new Company(2, "oocl", 2, OOCLEmployees);
         companies.add(company1);
         companies.add(company2);
         return companies;
@@ -40,26 +40,44 @@ public class CompanyServiceTest {
     @Test
     void should_return_company_when_update_given_company_id() {
         //given
-        List<Employee> baiduEmployees=new ArrayList<>();
+        List<Employee> baiduEmployees = new ArrayList<>();
         Employee baiduEmployee1 = new Employee(5, "baidu1", 20, "male", 10000);
         Employee baiduEmployee2 = new Employee(6, "baidu2", 20, "male", 11000);
         baiduEmployees.add(baiduEmployee1);
         baiduEmployees.add(baiduEmployee2);
-        Company newCompany=new Company(1,"baidu",2,baiduEmployees);
+        Company newCompany = new Company(1, "baidu", 2, baiduEmployees);
 
         CompanyRepository mockedCompanyRepository = Mockito.mock(CompanyRepository.class);
         when(mockedCompanyRepository.findById(1)).thenReturn(
                 Optional.of(generateCompanyList().get(0))
         );
         CompanyService companyService = new CompanyService(mockedCompanyRepository);
-        Integer companyId=1;
+        Integer companyId = 1;
         //when
-        Company returnedCompany=companyService.update(companyId,newCompany);
+        Company returnedCompany = companyService.update(companyId, newCompany);
         //then
         assertEquals(newCompany.getId(), returnedCompany.getId());
         assertEquals(newCompany.getCompanyName(), returnedCompany.getCompanyName());
         assertEquals(newCompany.getEmployeesNumber(), returnedCompany.getEmployeesNumber());
         assertEquals(newCompany.getEmployees(), returnedCompany.getEmployees());
+    }
+
+    @Test
+    void should_throw_NoSuchDataException_when_update_given_wrong_company_id() {
+        //given
+        Integer wrong_id = 999;
+
+        CompanyRepository mockedCompanyRepository = Mockito.mock(CompanyRepository.class);
+        CompanyService companyService = new CompanyService((mockedCompanyRepository));
+        when(mockedCompanyRepository.findById(wrong_id)).thenReturn(
+                Optional.ofNullable(null)
+        );
+
+        //when
+        Throwable throwable = assertThrows(NoSuchDataException.class, () -> companyService.update(wrong_id, new Company()));
+
+        //then
+        assertEquals(NoSuchDataException.class, throwable.getClass());
     }
 
     @Test
@@ -97,7 +115,7 @@ public class CompanyServiceTest {
     void should_return_specified_page_companies_when_get_companies_by_page_given_page_and_page_size() {
         //given
         CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
-        when(companyRepository.findAll(PageRequest.of(1,2))).thenReturn(new PageImpl<>(generateCompanyList()));
+        when(companyRepository.findAll(PageRequest.of(1, 2))).thenReturn(new PageImpl<>(generateCompanyList()));
         CompanyService companyService = new CompanyService(companyRepository);
         int page = 1;
         int pageSize = 2;
@@ -110,30 +128,30 @@ public class CompanyServiceTest {
     }
 
     @Test
-    void should_return_success_message_when_delete_company_given_company_name(){
+    void should_return_success_message_when_delete_company_given_company_name() {
         //given
         CompanyRepository mockedCompanyRepository = Mockito.mock(CompanyRepository.class);
         when(mockedCompanyRepository.findAll()).thenReturn(
                 generateCompanyList()
         );
         CompanyService companyService = new CompanyService(mockedCompanyRepository);
-        Integer companyId=1;
+        Integer companyId = 1;
         //when
         Boolean result = companyService.deleteCompanyById(companyId);
 
         //then
-        assertEquals(true,result);
+        assertEquals(true, result);
     }
 
     @Test
-    void should_return_new_company_when_add_given_new_company(){
+    void should_return_new_company_when_add_given_new_company() {
         //given
-        List<Employee> baiduEmployees=new ArrayList<>();
+        List<Employee> baiduEmployees = new ArrayList<>();
         Employee baiduEmployee1 = new Employee(5, "baidu1", 20, "male", 10000);
         Employee baiduEmployee2 = new Employee(6, "baidu2", 20, "male", 11000);
         baiduEmployees.add(baiduEmployee1);
         baiduEmployees.add(baiduEmployee2);
-        Company newCompany=new Company(3,"baidu",2,baiduEmployees);
+        Company newCompany = new Company(3, "baidu", 2, baiduEmployees);
 
         CompanyRepository mockedCompanyRepository = Mockito.mock(CompanyRepository.class);
         when(mockedCompanyRepository.save(newCompany)).thenReturn(
@@ -141,10 +159,10 @@ public class CompanyServiceTest {
         );
         CompanyService companyService = new CompanyService(mockedCompanyRepository);
         //when
-        Company returnedCompany=companyService.addCompany(newCompany);
+        Company returnedCompany = companyService.addCompany(newCompany);
         //then
         assertNotNull(returnedCompany);
-        assertEquals(returnedCompany,newCompany);
+        assertEquals(returnedCompany, newCompany);
     }
 
 }
