8c5455a72cfdfd68a1a7ad89f47372057a0f6d95
GAOGA4 <961977096@qq.com>
2020-07-30 09:12:23 +0800

    [Gavin] feature:complete CompanyController

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,7 +1,11 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.Service.CompanyService;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -12,80 +16,43 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
-
-    private static final String ALIBABA = "alibaba";
-    private static final String OOCL = "oocl";
-    private static List<Company> companies=new ArrayList<>();
-    public List<Company> getAllData(){
-        Employee employee1=new Employee(1,"alibaba1",17,"male",6000);
-        Employee employee2=new Employee(2,"alibaba2",18,"female",7000);
-        Employee employee3=new Employee(3,"oocl1",19,"male",8000);
-        Employee employee4=new Employee(4,"oocl2",20,"female",9000);
-        List<Employee> ALIBABAEmployees=new ArrayList<>();
-        ALIBABAEmployees.add(employee1);
-        ALIBABAEmployees.add(employee2);
-        List<Employee> OOCLEmployees=new ArrayList<>();
-        OOCLEmployees.add(employee3);
-        OOCLEmployees.add(employee4);
-        Company company1=new Company(1,ALIBABA,2,ALIBABAEmployees);
-        Company company2=new Company(2,OOCL,2,OOCLEmployees);
-        if(companies.size()==0){
-            companies.add(company1);
-            companies.add(company2);
-        }
-        return companies;
-    }
+    @Autowired
+    CompanyService companyService;
 
     @GetMapping("/{companyName}")
+    @ResponseStatus(HttpStatus.OK)
     public Company getSpecifiedNameCompany(@PathVariable String companyName){
-        return getAllData().stream().filter(c ->c.getCompanyName().equals(companyName)).collect(Collectors.toList()).get(0);
+        return companyService.findByName(companyName);
     }
     @GetMapping("/{companyName}/employees")
+    @ResponseStatus(HttpStatus.OK)
     public List<Employee> getSpecifiedNameCompanyEmployees(@PathVariable String companyName){
-        Company specifiedNameCompany=getSpecifiedNameCompany(companyName);
-        return specifiedNameCompany.getEmployees();
+        return companyService.findByName(companyName).getEmployees();
     }
 
-    @GetMapping
-    public List<Company> getCompanies(@RequestParam(defaultValue="null") String page,@RequestParam(defaultValue="null") String pageSize){
-        if(!page.equals("null")&&!pageSize.equals("null")){
-            return getAllData().stream().skip((Integer.parseInt(page)-1)*Integer.parseInt(pageSize)).limit(Integer.parseInt(pageSize)).collect(Collectors.toList());
-        }else{
-            return getAllData();
-        }
 
+    @GetMapping(params = {"page","pageNum"})
+    @ResponseStatus(HttpStatus.OK)
+    public Page<Company> getCompanies(@RequestParam Integer page, @RequestParam Integer pageSize){
+        return companyService.getCompanyByPage(page,pageSize);
     }
 
-    @PutMapping("/{companyName}")
-    public void updateCompany(@PathVariable String companyName,String newCompanyName,Integer newEmployeesNumber,List<Employee> employees){
-       Company specifiedCompany= getSpecifiedNameCompany(companyName);
-       if(newCompanyName!=null){
-           specifiedCompany.setCompanyName(newCompanyName);
-       }
-        if(newEmployeesNumber!=null){
-            specifiedCompany.setEmployeesNumber(newEmployeesNumber);
-        }
-        if(employees!=null){
-            specifiedCompany.setEmployees(employees);
-        }
+    @PutMapping("/{companyId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Company updateCompany(@PathVariable Integer companyId,@RequestBody Company newCompany){
+       return companyService.update(companyId,newCompany);
     }
 
     @PostMapping
-    public List<Company> addCompany(){
-        List<Employee> baiduEmployees=new ArrayList<>();
-        Employee baiduEmployee1 = new Employee(5, "baidu1", 20, "male", 10000);
-        Employee baiduEmployee2 = new Employee(6, "baidu2", 20, "male", 11000);
-        baiduEmployees.add(baiduEmployee1);
-        baiduEmployees.add(baiduEmployee2);
-        Company company=new Company(3,"baidu",2,baiduEmployees);
-        getAllData().add(company);
-        return getAllData();
+    @ResponseStatus(HttpStatus.OK)
+    public Company addCompany(Company company){
+        return companyService.addCompany(company);
     }
 
-    @DeleteMapping("/{companyName}")
-    public List<Company> deleteCompany(@PathVariable String companyName){
-        getAllData().stream().filter(company -> company.getCompanyName().equals(companyName)).collect(Collectors.toList()).get(0).setEmployees(null);
-        return getAllData();
+    @DeleteMapping("/{companyId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteCompany(@PathVariable Integer companyId){
+        companyService.deleteCompanyById(companyId);
     }
 
 }
