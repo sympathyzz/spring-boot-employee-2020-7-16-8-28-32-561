dab944b6fcc21227e143f4b52d186f4719ddacaf
GAOGA4 <961977096@qq.com>
2020-08-01 17:18:53 +0800

    [Gavin] refactor:add dto in EmployeeController

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,14 +1,20 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.Service.EmployeeService;
+import com.thoughtworks.springbootemployee.dto.CompanyDto;
+import com.thoughtworks.springbootemployee.dto.EmployeeDto;
+import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
@@ -17,46 +23,55 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
+    @Autowired
+    ModelMapper modelMapper;
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<Employee> getEmployees(){
-        return employeeService.getAll();
+    public List<EmployeeDto> getEmployees(){
+        return employeeService.getAll().stream().map(this::employeeConvertToDto).collect(Collectors.toList());
     }
 
     @GetMapping(params = {"page","pageNum"})
     @ResponseStatus(HttpStatus.OK)
-    public Page<Employee> getEmployeesByPage(@RequestParam Integer page, @RequestParam Integer pageSize){
-            return employeeService.getEmployeeByPage(page,pageSize);
+    public Page<EmployeeDto> getEmployeesByPage(@RequestParam Integer page, @RequestParam Integer pageSize){
+        return new PageImpl<>(employeeService.getEmployeeByPage(page,pageSize).stream().map(this::employeeConvertToDto).collect(Collectors.toList()));
+
     }
 
     @GetMapping(params = "gender")
     @ResponseStatus(HttpStatus.OK)
-    public List<Employee> getEmployeesByGender(@RequestParam String gender){
-        return employeeService.getEmployeeByGender(gender);
+    public List<EmployeeDto> getEmployeesByGender(@RequestParam String gender){
+        return employeeService.getEmployeeByGender(gender).stream().map(this::employeeConvertToDto).collect(Collectors.toList());
     }
 
     @GetMapping("/{employeeId}")
     @ResponseStatus(HttpStatus.OK)
-    public Employee getSpecifiedIdEmployee(@PathVariable Integer employeeId){
-        return employeeService.findById(employeeId);
+    public EmployeeDto getSpecifiedIdEmployee(@PathVariable Integer employeeId){
+        return employeeConvertToDto(employeeService.findById(employeeId));
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee addEmployee(@RequestBody Employee employee){
-        return  employeeService.addEmployee(employee);
+    public EmployeeDto addEmployee(@RequestBody Employee employee){
+        return  employeeConvertToDto(employeeService.addEmployee(employee));
     }
 
     @PutMapping("/{employeeId}")
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee updateEmployee(@PathVariable Integer employeeId,@RequestBody Employee employee) throws Exception {
-        return employeeService.update(employeeId,employee);
+    public EmployeeDto updateEmployee(@PathVariable Integer employeeId,@RequestBody Employee employee) throws Exception {
+        return employeeConvertToDto(employeeService.update(employeeId,employee));
     }
 
     @DeleteMapping("/{employeeId}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void deleteEmployee(@PathVariable Integer employeeId){
         employeeService.deleteEmployeeById(employeeId);
+    }
+
+    private EmployeeDto employeeConvertToDto(Employee employee) {
+        EmployeeDto employeeDto=modelMapper.map(employee,EmployeeDto.class);
+        return employeeDto;
     }
 
 }
